ea082285e868824e0694fa17844c2d8cdd51aaba
Simon Phillips <sxphillips@marketforce.com>
2020-01-22 21:15:25 -0500

    Updated gitignore

---
+++ storybook-static/precache-manifest.52f582f78a531e4f113e5e46095c0452.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5711692a496f9a89687188f4557e9554",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d7fdcbc90e59ee484623.bundle.js"
+  },
+  {
+    "url": "runtime~main.d7fdcbc90e59ee484623.bundle.js"
+  },
+  {
+    "url": "vendors~main.d7fdcbc90e59ee484623.bundle.js"
+  },
+  {
+    "url": "vendors~main.d7fdcbc90e59ee484623.bundle.js.LICENSE"
+  }
+]);
